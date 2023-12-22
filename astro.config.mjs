9d5ef5f126476e1a9dcbb357c19ae1d0348f455d
ChrisCass <christian_r4141@outlook.es>
2023-12-22 18:27:20 -0500

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://chrisiancasafranca.com',
+  site: 'https://christiancasafranca.com',
   integrations: [mdx(), sitemap(), tailwind()]
 });
